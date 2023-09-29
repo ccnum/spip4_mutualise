afccadea49e79af128bcff693b4ab2b0de32d900
paracine <pierrealexandreracine@gmail.com>
2023-09-29 13:36:38 +0200

    feat: test accès répertoire

--- src/shell_actions.sh
+++ src/shell_actions.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-mkdir -p /var/www/html/test/
-
-#for N in ${LISTE_CCN[@]} ; do
-#  echo "My name is $N"
-#  mkdir /var/www/html/$N
-#done
+for N in ${LISTE_CCN[@]} ; do
+  echo "My name is $N"
+  mkdir /var/www/html/$N
+done
