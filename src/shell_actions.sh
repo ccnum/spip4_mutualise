57cfbdbdbf71e4ca89d4038ecc67b98f29855e0e
paracine <pierrealexandreracine@gmail.com>
2023-10-05 09:45:52 +0200

    feat: création dynamique sous-répertoire des CCN

--- src/shell_actions.sh
+++ src/shell_actions.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 for CCN in $LISTE_CCN ; do
-  mkdir /var/www/html/sites/$CCN
+  mkdir -p /var/www/html/sites/$CCN/IMG
+  mkdir /var/www/html/sites/$CCN/tmp
+  mkdir /var/www/html/sites/$CCN/local
+  mkdir /var/www/html/sites/$CCN/config
 done
